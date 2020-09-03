faa8bfb9c956b1eb9f3f8f94b9598fe56cd7b8ac
Guilherme Palma <gomes.lmc@gmail.com>
2020-09-02 22:42:46 -0300

    Adiciona criptografia na persistencia do usuario e cria servico de auth

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,27 @@
+import { getRepository } from 'typeorm';
+import { hash, compare } from 'bcryptjs';
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<{ user: User }> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email password combination.');
+    }
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email password combination.');
+    }
+    return { user };
+  }
+}
+export default AuthenticateUserService;
