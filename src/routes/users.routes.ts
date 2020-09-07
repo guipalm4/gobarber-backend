efc52fcb4732149b01f5bbae1d095ffd2d49b55a
Guilherme Palma <gomes.lmc@gmail.com>
2020-09-07 00:29:32 -0300

    Adiciona handling de execptions

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -9,21 +9,17 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUsersService();
+  const createUser = new CreateUsersService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(user);
 });
 
 usersRouter.patch(
@@ -31,19 +27,15 @@ usersRouter.patch(
   ensureAuth,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
-      const user = await updateUserAvatar.execute({
-        userId: request.user.id,
-        avatarFileName: request.file.filename,
-      });
+    const updateUserAvatar = new UpdateUserAvatarService();
+    const user = await updateUserAvatar.execute({
+      userId: request.user.id,
+      avatarFileName: request.file.filename,
+    });
 
-      delete user.password;
+    delete user.password;
 
-      return response.json({ user });
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
+    return response.json({ user });
   },
 );
 
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -2,6 +2,7 @@ import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/ Appointment ';
 import AppointmentRepository from '../repositories/AppointmentsRepository';
+import AppError from '../errors/AppError';
 
 interface Request {
   providerId: string;
@@ -19,7 +20,7 @@ class CreateAppointmentService {
     );
 
     if (findAppointmentInSameDate) {
-      throw new Error('This appointment is already booked');
+      throw new AppError('This appointment is already booked', 400);
     }
 
     const appointment = appointmentRepository.create({
